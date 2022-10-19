3e354b4003dfe93279dc97a745fe7a6ba61b420d
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-10-18 21:40:57 -0300

    Inclusão de texto

---
+++ pages/contato.js
@@ -0,0 +1,10 @@
+
+export default function Contato(){
+  return(
+    <div>
+      <h1>
+        Pagina de contatos!
+      </h1>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ export default function Home() {
   return (
     <div>
       <h1>Aprendendo NextJS</h1>
+      <h2>@Sujeitoprogramador</h2>
     </div>
   )
 }
